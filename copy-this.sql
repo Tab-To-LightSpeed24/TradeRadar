87a61352fb6c39336f9a1c73e9dd0f3aa41af36f
[dyad] <kaushik.vgs@gmail.com>
2025-09-17 01:01:31 +0530

    [dyad] Adding the 'Bearer' prefix to the cron job's Authorization header - wrote 1 file(s)

--- copy-this.sql
+++ copy-this.sql
@@ -1,1 +1,10 @@
-SELECT cron.unschedule(4);
+SELECT cron.schedule(
+  'invoke-strategy-engine-every-5-minutes',
+  '*/5 * * * *',
+  $$
+  SELECT net.http_post(
+    url:='https://sdqwhzbeutetazvabjoq.supabase.co/functions/v1/strategy-engine',
+    headers:='{"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkcXdoemJldXRldGF6dmFiam9xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjIyOTI0MiwiZXhwIjoyMDcxODA1MjQyfQ.TD3nM99Oz5011EcW7_hoBM_zx58aCXSY82gie2OPMsM"}'
+  )
+  $$
+);
